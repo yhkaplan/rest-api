b6ad57df30d0937bb614a7cf906bca6b21419fc2
Joshua Kaplan <yhkaplan@gmail.com>
2018-03-15 23:40:10 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Go tutorial from: https://www.codementor.io/codehakase/building-a-restful-api-with-golang-a6yivzqdo
---
+++ main.go
@@ -0,0 +1,18 @@
+// Simple REST API tutorial
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
