33dd4e85bc4092713547e884b6baf7d13bcf1cb8
Joshua Kaplan <yhkaplan@gmail.com>
2018-03-19 23:41:26 +0900

    Finished

--- main.go
+++ main.go
@@ -43,15 +43,29 @@ func GetPerson(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for _, item := range people {
 		if item.ID == params["id"] {
-
+			json.NewEncoder(w).Encode(item)
+			return
 		}
 	}
+	json.NewEncoder(w).Encode(&Person{})
 }
 
 func CreatePerson(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 
 func DeletePerson(w http.ResponseWriter, r *http.Request) {
-
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(people)
 }
